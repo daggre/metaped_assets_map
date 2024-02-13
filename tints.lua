c0abe0a7720b3c4ab246591e4546a3a46d7e4eee
daggre_actual <dgract@gmail.com>
2024-02-12 22:50:36 -0800

    Raw dump

---
+++ tints.lua
@@ -0,0 +1,709 @@
+Tints = {
+    metaped_tint_combined_leather = {
+        {
+            tint0 = 102,
+            tint1 = 14,
+            tint2 = 82,
+        },
+        {
+            tint0 = 11,
+            tint1 = 17,
+            tint2 = 22,
+        },
+    },
+    metaped_tint_combined_leather1 = {
+        {
+            tint0 = 102,
+            tint1 = 14,
+            tint2 = 82,
+        },
+    },
+    metaped_tint_combined_leather2 = {
+        {
+            tint0 = 102,
+            tint1 = 14,
+            tint2 = 82,
+        },
+    },
+    metaped_tint_combined_leather3 = {
+        {
+            tint0 = 102,
+            tint1 = 14,
+            tint2 = 82,
+        },
+    },
+    metaped_tint_combined_leather4 = {
+        {
+            tint0 = 102,
+            tint1 = 14,
+            tint2 = 82,
+        },
+    },
+    metaped_tint_combined_leather5 = {
+        {
+            tint0 = 102,
+            tint1 = 14,
+            tint2 = 82,
+        },
+    },
+    metaped_tint_combined_leather6 = {
+        {
+            tint0 = 102,
+            tint1 = 14,
+            tint2 = 82,
+        },
+    },
+    metaped_tint_eye = {
+        {
+            tint0 = 1,
+            tint1 = 43,
+            tint2 = 45,
+        },
+    },
+    metaped_tint_generic = {
+        {
+            tint0 = 8,
+            tint1 = 96,
+            tint2 = 113,
+        },
+        {
+            tint0 = 119,
+            tint1 = 119,
+            tint2 = 119,
+        },
+        {
+            tint0 = 9,
+            tint1 = 118,
+            tint2 = 113,
+        },
+        {
+            tint0 = 117,
+            tint1 = 117,
+            tint2 = 117,
+        },
+        {
+            tint0 = 118,
+            tint1 = 118,
+            tint2 = 118,
+        },
+        {
+            tint0 = 9,
+            tint1 = 115,
+            tint2 = 113,
+        },
+        {
+            tint0 = 67,
+            tint1 = 64,
+            tint2 = 7,
+        },
+        {
+            tint0 = 8,
+            tint1 = 8,
+            tint2 = 113,
+        },
+        {
+            tint0 = 27,
+            tint1 = 27,
+            tint2 = 113,
+        },
+        {
+            tint0 = 102,
+            tint1 = 11,
+            tint2 = 113,
+        },
+        {
+            tint0 = 113,
+            tint1 = 113,
+            tint2 = 113,
+        },
+        {
+            tint0 = 11,
+            tint1 = 11,
+            tint2 = 113,
+        },
+        {
+            tint0 = 70,
+            tint1 = 70,
+            tint2 = 70,
+        },
+        {
+            tint0 = 64,
+            tint1 = 64,
+            tint2 = 7,
+        },
+        {
+            tint0 = 88,
+            tint1 = 88,
+            tint2 = 7,
+        },
+        {
+            tint0 = 140,
+            tint1 = 58,
+            tint2 = 30,
+        },
+        {
+            tint0 = 123,
+            tint1 = 66,
+            tint2 = 123,
+        },
+        {
+            tint0 = 17,
+            tint1 = 73,
+            tint2 = 141,
+        },
+        {
+            tint0 = 36,
+            tint1 = 147,
+            tint2 = 62,
+        },
+        {
+            tint0 = 64,
+            tint1 = 62,
+            tint2 = 143,
+        },
+        {
+            tint0 = 122,
+            tint1 = 148,
+            tint2 = 78,
+        },
+        {
+            tint0 = 25,
+            tint1 = 73,
+            tint2 = 141,
+        },
+        {
+            tint0 = 52,
+            tint1 = 128,
+            tint2 = 0,
+        },
+        {
+            tint0 = 19,
+            tint1 = 78,
+            tint2 = 146,
+        },
+        {
+            tint0 = 124,
+            tint1 = 153,
+            tint2 = 56,
+        },
+        {
+            tint0 = 1,
+            tint1 = 3,
+            tint2 = 5,
+        },
+        {
+            tint0 = 33,
+            tint1 = 33,
+            tint2 = 33,
+        },
+        {
+            tint0 = 33,
+            tint1 = 33,
+            tint2 = 33,
+        },
+        {
+            tint0 = 117,
+            tint1 = 117,
+            tint2 = 117,
+        },
+        {
+            tint0 = 0,
+            tint1 = 0,
+            tint2 = 0,
+        },
+        {
+            tint0 = 120,
+            tint1 = 120,
+            tint2 = 120,
+        },
+        {
+            tint0 = 2,
+            tint1 = 2,
+            tint2 = 2,
+        },
+        {
+            tint0 = 6,
+            tint1 = 6,
+            tint2 = 6,
+        },
+        {
+            tint0 = 4,
+            tint1 = 4,
+            tint2 = 4,
+        },
+        {
+            tint0 = 4,
+            tint1 = 4,
+            tint2 = 4,
+        },
+        {
+            tint0 = 10,
+            tint1 = 10,
+            tint2 = 10,
+        },
+        {
+            tint0 = 11,
+            tint1 = 11,
+            tint2 = 11,
+        },
+        {
+            tint0 = 25,
+            tint1 = 25,
+            tint2 = 25,
+        },
+        {
+            tint0 = 63,
+            tint1 = 63,
+            tint2 = 63,
+        },
+        {
+            tint0 = 27,
+            tint1 = 27,
+            tint2 = 27,
+        },
+        {
+            tint0 = 78,
+            tint1 = 78,
+            tint2 = 78,
+        },
+        {
+            tint0 = 6,
+            tint1 = 6,
+            tint2 = 6,
+        },
+        {
+            tint0 = 79,
+            tint1 = 79,
+            tint2 = 79,
+        },
+        {
+            tint0 = 0,
+            tint1 = 0,
+            tint2 = 0,
+        },
+        {
+            tint0 = 29,
+            tint1 = 29,
+            tint2 = 29,
+        },
+        {
+            tint0 = 2,
+            tint1 = 2,
+            tint2 = 2,
+        },
+        {
+            tint0 = 33,
+            tint1 = 33,
+            tint2 = 33,
+        },
+        {
+            tint0 = 72,
+            tint1 = 72,
+            tint2 = 72,
+        },
+        {
+            tint0 = 9,
+            tint1 = 118,
+            tint2 = 113,
+        },
+        {
+            tint0 = 63,
+            tint1 = 63,
+            tint2 = 63,
+        },
+        {
+            tint0 = 74,
+            tint1 = 74,
+            tint2 = 74,
+        },
+        {
+            tint0 = 7,
+            tint1 = 7,
+            tint2 = 7,
+        },
+        {
+            tint0 = 7,
+            tint1 = 7,
+            tint2 = 7,
+        },
+        {
+            tint0 = 9,
+            tint1 = 115,
+            tint2 = 113,
+        },
+        {
+            tint0 = 78,
+            tint1 = 78,
+            tint2 = 78,
+        },
+        {
+            tint0 = 76,
+            tint1 = 76,
+            tint2 = 76,
+        },
+        {
+            tint0 = 8,
+            tint1 = 8,
+            tint2 = 8,
+        },
+        {
+            tint0 = 8,
+            tint1 = 8,
+            tint2 = 8,
+        },
+        {
+            tint0 = 0,
+            tint1 = 0,
+            tint2 = 0,
+        },
+        {
+            tint0 = 90,
+            tint1 = 90,
+            tint2 = 90,
+        },
+        {
+            tint0 = 109,
+            tint1 = 109,
+            tint2 = 109,
+        },
+        {
+            tint0 = 79,
+            tint1 = 79,
+            tint2 = 79,
+        },
+        {
+            tint0 = 10,
+            tint1 = 10,
+            tint2 = 10,
+        },
+        {
+            tint0 = 2,
+            tint1 = 2,
+            tint2 = 2,
+        },
+        {
+            tint0 = 110,
+            tint1 = 114,
+            tint2 = 110,
+        },
+        {
+            tint0 = 112,
+            tint1 = 112,
+            tint2 = 112,
+        },
+        {
+            tint0 = 117,
+            tint1 = 117,
+            tint2 = 117,
+        },
+        {
+            tint0 = 53,
+            tint1 = 53,
+            tint2 = 53,
+        },
+        {
+            tint0 = 11,
+            tint1 = 11,
+            tint2 = 11,
+        },
+        {
+            tint0 = 70,
+            tint1 = 70,
+            tint2 = 70,
+        },
+        {
+            tint0 = 0,
+            tint1 = 114,
+            tint2 = 0,
+        },
+        {
+            tint0 = 120,
+            tint1 = 120,
+            tint2 = 120,
+        },
+        {
+            tint0 = 56,
+            tint1 = 56,
+            tint2 = 56,
+        },
+        {
+            tint0 = 25,
+            tint1 = 25,
+            tint2 = 25,
+        },
+        {
+            tint0 = 57,
+            tint1 = 57,
+            tint2 = 57,
+        },
+        {
+            tint0 = 57,
+            tint1 = 114,
+            tint2 = 57,
+        },
+        {
+            tint0 = 8,
+            tint1 = 96,
+            tint2 = 113,
+        },
+        {
+            tint0 = 27,
+            tint1 = 27,
+            tint2 = 27,
+        },
+        {
+            tint0 = 61,
+            tint1 = 61,
+            tint2 = 61,
+        },
+        {
+            tint0 = 88,
+            tint1 = 88,
+            tint2 = 7,
+        },
+        {
+            tint0 = 67,
+            tint1 = 64,
+            tint2 = 7,
+        },
+        {
+            tint0 = 88,
+            tint1 = 119,
+            tint2 = 63,
+        },
+        {
+            tint0 = 29,
+            tint1 = 29,
+            tint2 = 29,
+        },
+        {
+            tint0 = 64,
+            tint1 = 64,
+            tint2 = 7,
+        },
+        {
+            tint0 = 0,
+            tint1 = 0,
+            tint2 = 0,
+        },
+        {
+            tint0 = 33,
+            tint1 = 62,
+            tint2 = 64,
+        },
+        {
+            tint0 = 2,
+            tint1 = 2,
+            tint2 = 2,
+        },
+        {
+            tint0 = 89,
+            tint1 = 88,
+            tint2 = 88,
+        },
+        {
+            tint0 = 4,
+            tint1 = 4,
+            tint2 = 4,
+        },
+        {
+            tint0 = 119,
+            tint1 = 88,
+            tint2 = 118,
+        },
+        {
+            tint0 = 0,
+            tint1 = 86,
+            tint2 = 0,
+        },
+        {
+            tint0 = 0,
+            tint1 = 64,
+            tint2 = 0,
+        },
+    },
+    metaped_tint_hair = {
+        {
+            tint0 = 10,
+            tint1 = 10,
+            tint2 = 10,
+        },
+        {
+            tint0 = 14,
+            tint1 = 14,
+            tint2 = 14,
+        },
+        {
+            tint0 = 60,
+            tint1 = 60,
+            tint2 = 60,
+        },
+        {
+            tint0 = 57,
+            tint1 = 63,
+            tint2 = 63,
+        },
+        {
+            tint0 = 58,
+            tint1 = 62,
+            tint2 = 64,
+        },
+        {
+            tint0 = 59,
+            tint1 = 61,
+            tint2 = 61,
+        },
+        {
+            tint0 = 18,
+            tint1 = 15,
+            tint2 = 16,
+        },
+        {
+            tint0 = 17,
+            tint1 = 17,
+            tint2 = 17,
+        },
+        {
+            tint0 = 52,
+            tint1 = 62,
+            tint2 = 40,
+        },
+        {
+            tint0 = 25,
+            tint1 = 61,
+            tint2 = 63,
+        },
+        {
+            tint0 = 50,
+            tint1 = 58,
+            tint2 = 61,
+        },
+        {
+            tint0 = 53,
+            tint1 = 3,
+            tint2 = 61,
+        },
+        {
+            tint0 = 34,
+            tint1 = 34,
+            tint2 = 13,
+        },
+        {
+            tint0 = 52,
+            tint1 = 51,
+            tint2 = 53,
+        },
+        {
+            tint0 = 30,
+            tint1 = 53,
+            tint2 = 52,
+        },
+        {
+            tint0 = 53,
+            tint1 = 5,
+            tint2 = 52,
+        },
+        {
+            tint0 = 44,
+            tint1 = 44,
+            tint2 = 44,
+        },
+        {
+            tint0 = 26,
+            tint1 = 55,
+            tint2 = 56,
+        },
+        {
+            tint0 = 1,
+            tint1 = 4,
+            tint2 = 5,
+        },
+        {
+            tint0 = 8,
+            tint1 = 5,
+            tint2 = 6,
+        },
+        {
+            tint0 = 31,
+            tint1 = 61,
+            tint2 = 61,
+        },
+        {
+            tint0 = 8,
+            tint1 = 7,
+            tint2 = 6,
+        },
+        {
+            tint0 = 0,
+            tint1 = 5,
+            tint2 = 3,
+        },
+        {
+            tint0 = 9,
+            tint1 = 9,
+            tint2 = 9,
+        },
+    },
+    metaped_tint_hat = {
+        {
+            tint0 = 23,
+            tint1 = 109,
+            tint2 = 110,
+        },
+    },
+    metaped_tint_horse = {
+        {
+            tint0 = 23,
+            tint1 = 109,
+            tint2 = 110,
+        },
+    },
+    metaped_tint_horse_leather = {
+        {
+            tint0 = 23,
+            tint1 = 109,
+            tint2 = 110,
+        },
+    },
+    metaped_tint_leather = {
+        {
+            tint0 = 23,
+            tint1 = 27,
+            tint2 = 27,
+        },
+        {
+            tint0 = 4,
+            tint1 = 9,
+            tint2 = 13,
+        },
+        {
+            tint0 = 21,
+            tint1 = 21,
+            tint2 = 15,
+        },
+        {
+            tint0 = 2,
+            tint1 = 10,
+            tint2 = 102,
+        },
+    },
+    metaped_tint_makeup = {
+        {
+            tint0 = 0,
+            tint1 = 0,
+            tint2 = 0,
+        },
+        {
+            tint0 = 0,
+            tint1 = 0,
+            tint2 = 0,
+        },
+    },
+    metaped_tint_mpadv = {
+        {
+            tint0 = 10,
+            tint1 = 10,
+            tint2 = 10,
+        },
+    },
+}
